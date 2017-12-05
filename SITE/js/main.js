792072cedd834a419200b25b6fc69c54e97780d4
ozidrice <trolarddamien@gmail.com>
2017-12-05 11:57:25 +0100

    Ajout fonctions base API en JS

---
+++ SITE/js/main.js
@@ -0,0 +1,68 @@
+
+
+var API_link = "https://voxpopuliapi.ozidrice.com/";
+
+
+
+var user = create_user("pseudorandom2");
+//POUR RECUP LES VALEURS = user.then(val => console.log(val));
+var list_participants = list_user();
+var current_game = get_current_game().then(val=>console.log(val));
+
+
+function create_user(pseudo){
+	var url = API_link + "create_user";
+	var varnames = ["token","pseudo"];
+	var data = [publictoken,pseudo];
+	url = createlink(url,data,varnames);
+
+	return fetch_link(url);
+}
+
+function list_user(){
+	var url = API_link + "list_user";
+	var varnames = ["token"];
+	var data = [publictoken];
+	url = createlink(url,data,varnames);
+
+	return fetch_link(url);
+}
+
+function get_current_game(){
+	var url = API_link + "get_current_game";
+	var varnames = ["token"];
+	var data = [publictoken];
+	url = createlink(url,data,varnames);
+
+	return fetch_link(url);
+}
+
+/*SI ETAT == 1*/
+function join_game(idJoueur){
+	var url = API_link + "join_game";
+	var varnames = ["token","idJoueur"];
+	var data = [publictoken,idJoueur];
+	url = createlink(url,data,varnames);
+
+	return fetch_link(url);
+}
+
+function fetch_link(link){
+	return fetch(link)
+	.then((response) => response.json())
+	.then((responseData) => {
+		return responseData;
+	})
+	.catch(error => console.warn(error));
+}
+
+function createlink(baselink, datas, varnames){
+	if(!Array.isArray(datas))
+		return false
+
+	baselink+="?";
+	datas.forEach(function(data,key){
+		baselink+=varnames[key]+"="+data+"&";
+	});
+	return baselink;
+}
